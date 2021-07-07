2722d29599afedd91454f42f8d4b0fdd367063a9
Steff Beckers <steff@steffbeckers.eu>
2021-07-07 23:07:44 +0200

    Send email job added

---
+++ aspnet-core/src/Snooker.Application/BackgroundJobs/SendEmailJob.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Emailing;
+
+namespace Snooker.BackgroundJobs
+{
+    public class SendEmailJob : AsyncBackgroundJob<SendEmailJobArgs>, ITransientDependency
+    {
+        private readonly IEmailSender _emailSender;
+
+        public SendEmailJob(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public override async Task ExecuteAsync(SendEmailJobArgs args)
+        {
+            await _emailSender.SendAsync(args.Email, args.Subject, args.Body);
+        }
+    }
+}
