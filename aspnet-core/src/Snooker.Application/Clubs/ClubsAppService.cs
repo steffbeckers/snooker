0e82a5da4e3035a9f942121a1f8a58aedb95c4a3
Steff Beckers <steff@steffbeckers.eu>
2021-07-07 19:15:43 +0200

    Clubs app service update

--- aspnet-core/src/Snooker.Application/Clubs/ClubsAppService.cs
+++ aspnet-core/src/Snooker.Application/Clubs/ClubsAppService.cs
@@ -44,15 +44,6 @@ namespace Snooker.Clubs
 
         public virtual async Task<PagedResultDto<ClubSimpleDto>> GetListAsync(GetClubsInput input)
         {
-            //long totalCount = await _clubRepository.GetCountAsync(input.FilterText, input.Name);
-            //List<Club> clubs = await _clubRepository.GetListAsync(input.FilterText, input.Name, input.Sorting, input.MaxResultCount, input.SkipCount);
-
-            //return new PagedResultDto<ClubDto>
-            //{
-            //    TotalCount = totalCount,
-            //    Items = ObjectMapper.Map<List<Club>, List<ClubDto>>(clubs)
-            //};
-
             long totalCount = await _clubRepository.GetCountAsync(input.FilterText, input.Name);
             IQueryable<Club> clubQueryable = await _clubRepository.GetFilteredQueryableAsync(
                 input.FilterText,
