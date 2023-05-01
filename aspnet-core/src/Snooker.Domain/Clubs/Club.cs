14fe44816e472a863bfc0de42a889f50dfe83c1c
Steff Beckers <steff@steffbeckers.eu>
2023-05-01 19:23:28 +0200

    Club aggregate root

---
+++ aspnet-core/src/Snooker.Domain/Clubs/Club.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace Snooker.Clubs;
+
+public class Club : FullAuditedAggregateRoot<Guid>, IMultiTenant
+{
+	private string _name;
+
+	internal Club(Guid id, string name)
+	{
+		Id = id;
+		Name = name;
+	}
+
+	private Club()
+	{
+	}
+
+	public string Name
+	{
+		get => _name;
+		set
+		{
+			_name = value;
+		}
+	}
+
+	public Guid? TenantId { get; set; }
+}
