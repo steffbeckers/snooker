da35908f957a264eefd17ef4a2188f902c9b142e
Steff Beckers <steff@steffbeckers.eu>
2023-07-30 17:27:29 +0200

    Number of tables

---
+++ data/Limburg/NumberOfTablesPerClub.md
@@ -0,0 +1,12 @@
+# Number of tables per club
+
+- Biljart Lounge: 4
+- Buckingham: 8
+- De Kreeft: 6
+- De Maxx: 10
+- Happy Snooker: 5
+- NRG: 3
+- Re-Spot: 6
+- Riley Inn: 8
+- Snooker Sports: 3
+- Zuma: 6
--- data/SQL/Interclub/Clubs.sql
+++ data/SQL/Interclub/Clubs.sql
@@ -4,10 +4,11 @@ SELECT --c.[Id]
       ,c.[Email]
       ,c.[PhoneNumber]
       ,c.[Website]
-	  ,a.[Street]
+      ,a.[Street]
       ,a.[Number]
       ,a.[PostalCode]
       ,a.[City]
+      ,c.[NumberOfTables]
   FROM [dbo].[InterclubClubs] c
   LEFT JOIN [dbo].[InterclubClubAddresses] a ON c.Id = a.ClubId
   ORDER BY c.[Name]
