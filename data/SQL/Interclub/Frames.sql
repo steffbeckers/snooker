d9cbf60e9ef5cbcac9b78e1d7f1cefddb3b8bd07
Steff Beckers <steff@steffbeckers.eu>
2023-07-30 12:59:23 +0200

    SQL query updates

--- data/SQL/Interclub/Frames.sql
+++ data/SQL/Interclub/Frames.sql
@@ -3,9 +3,6 @@ SELECT hp.[FirstName] + ' ' + hp.[LastName] AS [HomePlayerName]
       ,f.[AwayPlayerScore]
       ,ap.[FirstName] + ' ' + ap.[LastName] AS [AwayPlayerName]
   FROM [dbo].[InterclubFrames] f
-  LEFT JOIN [dbo].[InterclubMatches] m ON f.[MatchId] = m.[Id]
-  LEFT JOIN [dbo].[InterclubMatchTeamPlayers] mhtp ON f.[HomePlayerId] = mhtp.[Id]
-  LEFT JOIN [dbo].[InterclubPlayers] hp ON mhtp.[PlayerId] = hp.[Id]
-  LEFT JOIN [dbo].[InterclubMatchTeamPlayers] matp ON f.[AwayPlayerId] = matp.[Id]
-  LEFT JOIN [dbo].[InterclubPlayers] ap ON matp.[PlayerId] = ap.[Id]
+  LEFT JOIN [dbo].[InterclubPlayers] hp ON f.[HomePlayerId] = hp.[Id]
+  LEFT JOIN [dbo].[InterclubPlayers] ap ON f.[AwayPlayerId] = ap.[Id]
   ORDER BY f.[HomePlayerScore] DESC
--- data/SQL/Interclub/Matches.sql
+++ data/SQL/Interclub/Matches.sql
@@ -1,9 +1,14 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT [DivisionId]
-      ,[Date]
-      ,[HomeTeamId]
-      ,[HomeTeamScore]
-      ,[AwayTeamScore]
-      ,[AwayTeamId]
-  FROM [dbo].[InterclubMatches]
-  ORDER BY [Date]
+SELECT d.[Name] AS [DivisionName]
+      ,m.[Date]
+      ,htc.[Name] + ' ' + ht.[Name] AS [HomeTeamName]
+      ,m.[HomeTeamScore]
+      ,m.[AwayTeamScore]
+	  ,atc.[Name] + ' ' + at.[Name] AS [AwayTeamName]
+  FROM [dbo].[InterclubMatches] m
+  LEFT JOIN [dbo].[InterclubDivisions] d ON m.[DivisionId] = d.[Id]
+  LEFT JOIN [dbo].[InterclubTeams] ht ON m.[HomeTeamId] = ht.[Id]
+  LEFT JOIN [dbo].[InterclubClubs] htc ON ht.[ClubId] = htc.[Id]
+  LEFT JOIN [dbo].[InterclubTeams] at ON m.[AwayTeamId] = at.[Id]
+  LEFT JOIN [dbo].[InterclubClubs] atc ON at.[ClubId] = atc.[Id]
+  WHERE htc.[Name] = 'NRG' AND ht.[Name] = 'C' OR atc.[Name] = 'NRG' AND at.[Name] = 'C'
+  ORDER BY d.[SortOrder], m.[Date]
